038b4c2f27c78bf4977f53c2de88abc314efbecf
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-01-19 19:35:52 +0700

    Add project files.

---
+++ ServiceTIXMovie/IService1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceTIXMovie
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string transaksitiket(int IDFilm, string Bioskop, string NamaCustomer, string Phone, int Studio, string Tanggal, string JamTayang, string Row, string Seat, string Harga, string Total, string Status);
+        [OperationContract]
+        string edittransaksitiket(int IDTransaksi, string Status);
+        [OperationContract]
+        string deletetransaksitiket(string NamaCustomer);
+        [OperationContract]
+        string film(string JudulFilm, string Deskripsi, string Harga);
+        [OperationContract]
+        string editfilm(int id, string Harga);
+        [OperationContract]
+        string deletefilm(string JudulFilm);
+        [OperationContract]
+        string statustiket(int IDStatus, string IDTransaksi);
+
+        [OperationContract]
+        List<DataTransaksi> DataTransaksi();
+        [OperationContract]
+        List<DataFilm> DataFilm();
+        [OperationContract]
+        List<StatusTiket> StatusTiket();
+
+        [OperationContract]
+        string Login(string email, string password);
+        [OperationContract]
+        string Register(string username, string email, string phone, string password, string role);
+        [OperationContract]
+        List<DataRegister> DataRegist();
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceTIXMovie.ContractType".
+    [DataContract]
+    public class DataTransaksi
+    {
+        [DataMember(Order = 1)]
+        public int IDTransaksi { get; set; }
+        [DataMember(Order = 2)]
+        public int Film { get; set; } // Method
+        [DataMember(Order = 3)]
+        public string Bioskop { get; set; }
+        [DataMember(Order = 4)]
+        public string NamaCustomer { get; set; }
+        [DataMember(Order = 5)]
+        public string Phone { get; set; }
+        [DataMember(Order = 6)]
+        public int Studio { get; set; }
+        [DataMember(Order = 7)]
+        public string Tanggal { get; set; }
+        [DataMember(Order = 8)]
+        public string JamTayang { get; set; }
+        [DataMember(Order = 9)]
+        public string Row { get; set; }
+        [DataMember(Order = 10)]
+        public string Seat { get; set; }
+        [DataMember(Order = 11)]
+        public string Harga { get; set; }
+        [DataMember(Order = 12)]
+        public string Total { get; set; }
+        [DataMember(Order = 13)]
+        public string Status { get; set; }
+    }
+
+    [DataContract]
+    public class DataFilm 
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string JudulFilm { get; set; }
+        [DataMember(Order = 3)]
+        public string Deskripsi { get; set; }
+        [DataMember(Order = 4)]
+        public string Harga { get; set; }
+    }
+
+    [DataContract]
+    public class StatusTiket
+    {
+        [DataMember]
+        public int IDStatus { get; set; }
+        [DataMember]
+        public string IDTransaksi { get; set; }
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string email { get; set; }
+        [DataMember(Order = 4)]
+        public string phone { get; set; }
+        [DataMember(Order = 5)]
+        public string password { get; set; }
+        [DataMember(Order = 6)]
+        public string role { get; set; }
+    }
+}
---
+++ ServiceTIXMovie/Service1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceTIXMovie
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        string constring = "Data Source=LAPTOP-GVH5477F;Initial Catalog=TIXMovie;Persist Security Info=true;User ID=sa;Password=bima071200";
+        SqlConnection connection;
+        SqlCommand com;
+
+        public List<DataRegister> DataRegist()
+        {
+            List<DataRegister> list = new List<DataRegister>();
+            try
+            {
+                string sql = "select ID_User, Username, Email, Phone, Password, Role from UserAccess";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    DataRegister data = new DataRegister();
+                    data.id = reader.GetInt32(0);
+                    data.username = reader.GetString(1);
+                    data.email = reader.GetString(2);
+                    data.phone = reader.GetString(3);
+                    data.password = reader.GetString(4);
+                    data.role = reader.GetString(5);
+                    list.Add(data);
+                }
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+            return list;
+        }
+
+        public string deletefilm(string JudulFilm)
+        {
+            try
+            {
+                int id = 0;
+                string sql = "select ID_Film from dbo.Film where Judul_Film = '" + JudulFilm + "'";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+
+                connection.Close();
+                string sql2 = "delete from Film where ID_Film = " + id + "";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql2, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+        }
+
+        public string deletetransaksitiket(string NamaCustomer)
+        {
+            string a = "gagal";
+
+            try
+            {
+                string sql = "delete from dbo.TransaksiTicket where ID_transaksi = '" + NamaCustomer + "'";
+                connection = new SqlConnection(constring); 
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                a = "sukses";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return a;
+        }
+
+        public string editfilm(int id, string Harga)
+        {
+            string a = "gagal";
+
+            try
+            {
+                string sql = "update dbo.Film set Harga = '" + Harga + "'where ID_Film = " + id + "";
+                connection = new SqlConnection(constring); 
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                a = "sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+
+            return a;
+        }
+
+        public string edittransaksitiket(int IDTransaksi, string Status)
+        {
+            string a = "gagal";
+
+            try
+            {
+                string sql = "update dbo.TransaksiTicket set Status = '" + Status + "''where ID_Film = " + IDTransaksi + "";
+                connection = new SqlConnection(constring); 
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                a = "sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+
+            return a;
+        }
+
+        public string film(string JudulFilm, string Deskripsi, string Harga)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.Film values ('" + JudulFilm + "', '" + Deskripsi + "', '" + Harga + "')"; 
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                a = "sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+            return a;
+        }
+
+        public List<DataFilm> DataFilm()
+        {
+            List<DataFilm> list = new List<DataFilm>();
+            try
+            {
+                string sql = "select ID_Film, Judul_Film, Deskripsi, Harga from dbo.Film";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    DataFilm data = new DataFilm();
+                    data.id = reader.GetInt32(0);
+                    data.JudulFilm = reader.GetString(1);
+                    data.Deskripsi = reader.GetString(2);
+                    data.Harga = reader.GetString(3);
+                    list.Add(data);
+                }
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+            return list;
+        }
+
+        public string Login(string email, string password)
+        {
+            string kategori = "";
+
+            string sql = "select role from UserAccess where Email = '" + email + "' and Password = '" + password + "'";
+            connection = new SqlConnection(constring);
+            com = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                kategori = reader.GetString(0);
+            }
+
+            return kategori;
+        }
+
+        public string Register(string username, string email, string phone, string password, string role)
+        {
+            try
+            {
+                string sql = "insert into UserAccess values ('" + username + "', '" + email + "', '" + phone + "', '" + password + "', '" + role + "')";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "Sukses";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public string statustiket(int IDStatus, string IDTransaksi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StatusTiket> StatusTiket()
+        {
+            List<StatusTiket> statusTikets = new List<StatusTiket>(); 
+            try
+            {
+                string sql = "select ID_Status, ID_Transaksi from dbo.StatusTiket";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection); 
+                connection.Open(); 
+                SqlDataReader reader = com.ExecuteReader(); 
+                while (reader.Read())
+                {
+                    /* Nama Class */
+                    StatusTiket data = new StatusTiket(); 
+                    // Bentuk array
+                    data.IDStatus = reader.GetInt32(0); 
+                    data.IDTransaksi = reader.GetString(1);
+                    statusTikets.Add(data); 
+                }
+                connection.Close(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+            }
+            return statusTikets;
+        }
+
+        public string transaksitiket(int IDFilm, string Bioskop, string NamaCustomer, string Phone, int Studio, string Tanggal, string JamTayang, string Row, string Seat, string Harga, string Total, string Status)
+        {
+            string a = "gagal";
+            try
+            {
+                string sql = "insert into dbo.TransaksiTicket values ('" + IDFilm + "', '" + Bioskop + "', '" + NamaCustomer + "', '" + Phone + "', '" + Studio + "', '" + Tanggal + "', '" + JamTayang + "', '" + Row + "', '" + Seat + "', '" + Harga + "', '" + Total + "', '" + Status + "')";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                a = "sukses";
+            }
+            catch (Exception es)
+            {
+                Console.WriteLine(es);
+            }
+            return a;
+        }
+
+        public List<DataTransaksi> DataTransaksi()
+        {
+            List<DataTransaksi> list = new List<DataTransaksi>(); 
+
+            try
+            {
+                string sql = "select ID_Transaksi, ID_Film, Bioskop, Nama_Customer, Phone, Studio, Tanggal, Jam_Tayang, Row, Seat, Harga, Total, Status from dbo.TransaksiTicket";
+                connection = new SqlConnection(constring); 
+                com = new SqlCommand(sql, connection); 
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader(); 
+                while (reader.Read())
+                {
+                    // Nama class
+                    DataTransaksi data = new DataTransaksi(); 
+
+                    // Bentuk array
+                    data.IDTransaksi = reader.GetInt32(0); 
+                    data.Film = reader.GetInt32(1);
+                    data.Bioskop = reader.GetString(2);
+                    data.NamaCustomer = reader.GetString(3);
+                    data.Phone = reader.GetString(4);
+                    data.Studio = reader.GetInt32(5);
+                    data.Tanggal = reader.GetString(6);
+                    data.JamTayang = reader.GetString(7);
+                    data.Row = reader.GetString(8);
+                    data.Seat = reader.GetString(9);
+                    data.Harga = reader.GetString(10);
+                    data.Total = reader.GetString(11);
+                    data.Status = reader.GetString(12);
+                    list.Add(data); 
+                }
+
+                connection.Close(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return list;
+        }
+    }
+}
